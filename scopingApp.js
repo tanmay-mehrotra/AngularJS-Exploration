9c623dc4544fc21e7888266de4d7cfb6d0a9cc64
Tanmay Mehrotra <tanmaymehrotra86@gmail.com>
2014-10-28 07:48:00 -0400

    moved files to folders

--- scopingApp.js
+++
@@ -1,8 +0,0 @@
-  angular.module('scoping', [])
-    .controller('OuterController', function($scope){
-      $scope.outer_var = 10;
-    
-    })
-    .controller('MiddleController', function($scope) {
-      $scope.middle_var=20;
-    });
